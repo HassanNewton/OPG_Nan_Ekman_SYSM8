3b67e58035ad6b4eb574fb0768b61ca479c4a436
NanEkman <nen_92@hotmail.com>
2024-10-17 18:16:09 +0200

    skapat splashscreen

--- FitTrack/App.xaml.cs
+++ FitTrack/App.xaml.cs
@@ -9,6 +9,24 @@ namespace FitTrack
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Skapa och visa splashskärmen
+            var splashScreen = new View.SplashScreen();
+            splashScreen.Show();
+
+            // Simulera en kort fördröjning (4 sekunder)
+            System.Threading.Thread.Sleep(4000);
+
+            // Öppna huvudfönstret
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            // Stäng splashskärmen
+            splashScreen.Close();
+        }
     }
 
 }
--- FitTrack/View/SplashScreen.xaml.cs
+++ FitTrack/View/SplashScreen.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FitTrack.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace FitTrack.View
         public SplashScreen()
         {
             InitializeComponent();
+            DataContext = new SplashScreenViewModel(); // Sätt DataContext till ViewModel
         }
     }
 }
