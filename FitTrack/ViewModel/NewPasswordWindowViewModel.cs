9e9d68aff3fe3fb77d33d3c0f77bb955aff0c523
NanEkman <nen_92@hotmail.com>
2024-10-21 11:26:03 +0200

    Tillfällig SecurityQuestion med MEssageBox implementerad

--- FitTrack/ViewModel/NewPasswordWindowViewModel.cs
+++ FitTrack/ViewModel/NewPasswordWindowViewModel.cs
@@ -10,6 +10,19 @@ namespace FitTrack.ViewModel
 {
     public class NewPasswordWindowViewModel : ViewModelBase
     {
+
+        private string userInput;
+
+        public string UserInput
+        {
+            get { return userInput; }
+            set
+            {
+                userInput = value;
+                OnPropertyChanged();
+            }
+        }
+
         private string passwordInput;
 
         public string PasswordInput
@@ -64,12 +77,54 @@ namespace FitTrack.ViewModel
             {
                 MessageBox.Show("Passwords does not match.");
             }
+            // Kontrollera om användarnamn, lösenord och confirm lösenord inte är tomma
+            if (string.IsNullOrEmpty(UserInput) || string.IsNullOrEmpty(PasswordInput) ||
+                string.IsNullOrEmpty(ConfirmPasswordInput))
+            {
+                MessageBox.Show("Please fill all the boxes.");
+                return;
+            }
+            else
+            {
+                // KOD-LOGIK
+
+                Application.Current.MainWindow.Close();
+                OpenMainWindow();
+            }
         }
 
         private void ValidateUserSecurityQuestion(object parameter)
         {
-            // Användaren kommer behöva svara på en säkerhetsfråga för att validera att det är rätt användare
-            MessageBox.Show("QUESTION");
+            // Användaren kommer behöva svara på en säkerhetsfråga för att validera att det är rätt användare   
+
+            // Tillfälligt med MessageBox
+            string message = "Was Alice your kindergarten teacher's name?";
+            string title = "Security Question";
+            MessageBoxButton buttons = MessageBoxButton.YesNo;
+
+            // Använd MessageBoxResult istället för DialogResult
+            MessageBoxResult result = MessageBox.Show(message, title, buttons);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                // Stäng fönstret om användaren svarade "Yes"
+                MessageBox.Show("Correct! Welcome!");
+            }
+            else
+            {
+                // Om användare svarar "No"
+                MessageBox.Show("Security check failed.");
+            }
+        }
+
+        private void OpenMainWindow()
+        {
+            // Skapa en ny instans av MainWindow
+            MainWindow mainWindow = new MainWindow();
+
+            // Sätt det nya fönstret som huvudfönster och visa det
+            Application.Current.MainWindow = mainWindow;
+            mainWindow.Show();
         }
     }
 }
