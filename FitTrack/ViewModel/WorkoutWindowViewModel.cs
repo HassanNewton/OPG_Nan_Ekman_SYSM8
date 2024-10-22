2367d4a75348566e9b2f4d73af3e143c1916bfd6
NanEkman <nen_92@hotmail.com>
2024-10-22 22:37:15 +0200

    Påbörjat Workoutwindow, men osäker på vad jag ska göra med knappen user som ska öppna UserDetailsWindow

--- FitTrack/ViewModel/WorkoutWindowViewModel.cs
+++ FitTrack/ViewModel/WorkoutWindowViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using FitTrack.Model;
 using FitTrack.MVVM;
+using FitTrack.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FitTrack.ViewModel
 {
@@ -13,18 +16,43 @@ namespace FitTrack.ViewModel
         // Egenskaper
         private User user;
 
+        public User User
+        {
+            get { return user; }
+            set 
+            { 
+                user = value; 
+                OnPropertyChanged(nameof(user));
+            }
+        }
+
+        // Lista som innehåller workout objekt
+        private List <Workout> WorkoutList { get; set; }
+
+        public RelayCommand AddWorkOutCommand { get; }
+        public RelayCommand OpenDetailsCommand { get; }
 
         // Konstruktor
         public WorkoutWindowViewModel()
         {
+            WorkoutList = new List<Workout>();
 
+            AddWorkOutCommand = new RelayCommand(AddWorkOut);
+            //OpenDetailsCommand = new RelayCommand(OpenDetails()); // funkar ej då jag har parameter Workout workout
         }
 
-
         // Metoder
-        private void AddWorkOut()
+        private void AddWorkOut(object parameter)
         {
+            // Skapa en ny instans av WorkoutWindow
+            UserDetailsWindow userDetailsWindow = new UserDetailsWindow();
 
+            // Stäng MainWindow
+            Application.Current.MainWindow.Close();
+
+            // Sätt det nya fönstret som huvudfönster och visa det
+            Application.Current.MainWindow = userDetailsWindow;
+            userDetailsWindow.Show();
         }
 
         private void RemoveWorkOut()
